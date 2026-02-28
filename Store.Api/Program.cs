b8b72b786ee413dc79d2ad9124c87d0f5731e89c
Omar <omareegab@gmail.com>
2026-02-27 20:19:30 -0500

    cambios

--- Store.Api/Program.cs
+++ Store.Api/Program.cs
@@ -1,6 +1,10 @@
+// Boostrap the application
+// Config de service of the application
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// what happen when we start the application
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
+// Host the application
+// Use builder pattern
